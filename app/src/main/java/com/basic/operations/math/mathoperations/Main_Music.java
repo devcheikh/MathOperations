2420f8985b719d58bc1cfb3040b0db916377c37a
Hamza Mubashir <hamza.mubashir2@gmail.com>
2016-06-15 14:50:13 -0400

    second commit

---
+++ app/src/main/java/com/basic/operations/math/mathoperations/Main_Music.java
@@ -0,0 +1,44 @@
+package com.basic.operations.math.mathoperations;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class Main_Music extends Service {
+
+    static MediaPlayer mp;
+
+    @Override
+    public void onCreate() {
+        mp = MediaPlayer.create(this, R.raw.background_main_music);
+        mp.setLooping(true);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (!mp.isPlaying()) {
+            mp.start();
+        }
+
+        return 1;
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        if (mp.isPlaying()) {
+            mp.stop();
+        }
+        mp.release();
+    }
+
+}
