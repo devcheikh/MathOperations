b70183315464eeab7325dfe2b662ccf73770b39d
Hamza Mubashir <hamza.mubashir2@gmail.com>
2016-06-15 14:51:49 -0400

    second commit

--- app/src/main/java/com/basic/operations/math/mathoperations/Game_Results_Fragment.java
+++
@@ -1,7 +0,0 @@
-package com.basic.operations.math.mathoperations;
-
-import android.app.Fragment;
-
-
-public class Game_Results_Fragment extends Fragment {
-}
--- app/src/main/java/com/basic/operations/math/mathoperations/Results_Interface.java
+++
@@ -1,5 +0,0 @@
-package com.basic.operations.math.mathoperations;
-
-public interface Results_Interface {
-    void processTotal_results(int total, String[][] total_array);
-}
